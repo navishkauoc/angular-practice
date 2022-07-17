a2abfb1a81249700fd9886c50af873cdca8c2236
navishkauoc <navishka.randika@gmail.com>
2022-07-17 23:23:38 +0530

    Load permissions with APP_INITIALIZER

--- 02) handling authorizations/handling-authorizations/src/app/app.module.ts
+++ 02) handling authorizations/handling-authorizations/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxPermissionsModule } from 'ngx-permissions';
+import { NgxPermissionsModule, NgxPermissionsService } from 'ngx-permissions';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -14,8 +14,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoadPermissionsService } from './service/load-permissions.service';
 
-
+export function permissionsFactory(
+  loadPermissionsService: LoadPermissionsService,
+  ngxPermissionsService: NgxPermissionsService) {
+  return () => {
+    return loadPermissionsService.loadPermissions().then((data) => {
+      ngxPermissionsService.loadPermissions(data)
+      return true
+    })
+  }
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,7 +44,12 @@ import { HttpClientModule } from '@angular/common/http';
     NgxPermissionsModule.forRoot(),
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: permissionsFactory,
+    deps: [LoadPermissionsService, NgxPermissionsModule],
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
